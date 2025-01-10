d0ec4934fc2141b1f81b3d73c833c8a2891cba8a
BeneX Drake <benex@hotmail.de>
2025-01-10 19:37:41 +0100

    update

--- components/article/article.tsx
+++ components/article/article.tsx
@@ -1,16 +1,16 @@
 import styles from '@/styles/modules/article.module.css';
 
-export let Article = (props:any) =>
-{
+export let Article = (props: any) => {
+
+
     const type = props.type;
     console.log(type)
 
     let a = "";
     let card = "";
-    let image = styles.image;
+    // let image = styles.image;
 
-    switch(type)
-    {
+    switch (type) {
         case 1:
             a = styles.a1;
             card = styles.card1;
@@ -20,24 +20,23 @@ export let Article = (props:any) =>
             card = styles.card2;
             break;
         case 3:
-            a = styles.a1;
+            a = styles.a3;
             card = styles.card3;
-            
     }
 
 
     return (
         <div className={card}>
-                <a href="" className={a}>
-                <div className={styles.image}  style={{backgroundImage:'url("https://placehold.co/600x400")'}}></div>
+            <a href="" className={a}>
+                <div className={styles.image} style={{ backgroundImage: 'url("https://placehold.co/600x400")' }}></div>
                 <div className={styles.content}>
-                    <span>Text</span>
-                    <h1></h1>
-                    <p></p>
+                    <span className={styles.date}>Sunday, 1 Jan 2023</span>
+                    <h3>UX review presentations</h3>
+                    <p className={styles.description}>How do you create compelling presantations that wow your colleagues and impress your managers?</p>
                     <ul className={styles.ul}>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li><span className={styles.tag}>Design</span></li>
+                        <li><span className={styles.tag}>Research</span></li>
+                        <li><span className={styles.tag}>Presantation</span></li>
                     </ul>
                 </div>
             </a>
