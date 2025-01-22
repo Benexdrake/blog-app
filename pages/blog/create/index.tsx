1bdd2c482a791c57832eaa2659639ca0bcf81686
BeneX Drake <benex@hotmail.de>
2025-01-22 09:50:50 +0100

    change route, old dont need anymore

--- pages/blog/create/index.tsx
+++ pages/blog/create/index.tsx
@@ -1,113 +1,121 @@
 import styles from '@/styles/modules/blog/create/create_blog.module.css'
-import { ChangeEvent, JSX, MouseEvent, useState } from 'react'
-import AddContent from '@/components/blog/add_content';
-import InputField from '@/components/blog/input_field';
-import PreviewArticle from '@/components/blog/preview_article'
+import { JSX, useState} from 'react'
 
-export default function CreateArticleHeader(param:any)
+import Header from '@/components/blog/create/header';
+import PreviewHeader from '@/components/blog/create/elements/header/preview_header';
+import PreviewBlogEntry from '@/components/blog/create/preview_blog_entry';
+import FloatMenu from '@/components/blog/create/float_menu';
+import CreateImage from '@/components/blog/create/elements/image/image';
+
+import { BlogElementConfig } from '@/types/blog_create_element';
+import PreviewImage from '@/components/blog/create/elements/image/preview_image';
+import CreateHeader from '@/components/blog/create/elements/header/header';
+import CreateContent from '@/components/blog/create/elements/content/content';
+import PreviewContent from '@/components/blog/create/elements/content/preview_content';
+
+
+export default function CreateArticle()
 {
-    const [headerImage, setHeaderImage] = useState('JS.jpg');
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-
-    const [tags, setTags] = useState<string[]>([]);
-
-    const [addBlock, setAddBlock] = useState<string[]>([])
-
-    const [content, setContent] = useState([]);
-
-    const headerImageHandler = (e: ChangeEvent<HTMLInputElement>) => 
-        {
-            if (!e.target.files) return;
+    const [elements, setElements] = useState<JSX.Element[]>([])
+    const [header, setHeader] = useState({image:'JS.jpg', title:'', description:'', tags:[]})
+    const [preview, setPreview] = useState(false);
     
-            const file = e.target.files[0];
-    
-            const reader = new FileReader();
-            
-            reader.readAsDataURL(file)
-    
-            reader.onload = () => 
-            {
-                const result = reader.result;
-                if (result)
-                {
-                    setHeaderImage(result as string)
-                }
-            }
-        }
-        
-        const titleHandler = (e: ChangeEvent<HTMLInputElement>) => 
-        {
-            setTitle(e.target.value)
-        }
-    
-        const descriptionHandler = (e: ChangeEvent<HTMLTextAreaElement>) => 
-        {
-            setDescription(e.target.value)
-        }
-
-    const tagHandler = (e: any) => 
-    {
-        e.target.children[0].checked = !e.target.children[0].checked;
-
-        if (e.target.children[0].checked) 
-        {
-            if(tags.length > 2)
-                return;
-            e.target.style.borderColor = 'green'
-            let tags_copy = [...tags];
-            tags_copy.push(e.target.innerText)
-            setTags(tags_copy)
-        }
-        else
-        {
-            e.target.style.borderColor = 'var(--color3)'
-            setTags(tags.filter((x:string) => x !== e.target.innerText))
-        }
-    }
-
-    const onClickAddBlock = () => 
-    {
+    const createElement = (type:string) =>
+    {   
         const id = crypto.randomUUID()
-        setAddBlock([...addBlock, id])
+        let config:BlogElementConfig = {id, type, options:{}, content:''}
+        let element;
+
+        switch(type)
+        {
+            case 'image':
+                config.options = {backgroundSize:'cover', backgroundPosition:'center', backgroundImage:'url(/assets/images/JS.jpg)', height:'200px', minWidth:'200px', margin:'16px 0'}
+                element = <CreateImage config={config} updateElement={() => {}} deleteElement={() => {}}/>
+                setElements([...elements, element])
+                break;
+            case 'header':
+                config.options = {padding:'16px'}
+                element = <CreateHeader config={config} updateElement={() => {}} deleteElement={() => {}}/>
+                setElements([...elements, element])
+                break;
+            case 'content':
+                config.options = {padding:'16px'}
+                element = <CreateContent config={config} updateElement={() => {}} deleteElement={() => {}}/>
+                break;
+            }
+
+            if(!element) return;
+
+            setElements([...elements, element])
+    }
+        
+    const updateElement = (config:BlogElementConfig) =>
+    {   
+        let index = elements.findIndex(x => x.props.config.id === config.id)
+
+        switch(config.type)
+        {
+            case'image':
+                elements[index] = <CreateImage config={config}  updateElement={() => {}} deleteElement={() => {}}/>
+                break;
+            case'header':
+                elements[index] = <CreateHeader config={config} updateElement={() => {}} deleteElement={() => {}}/>
+                break;
+            case'content':
+                elements[index] = <CreateContent config={config} updateElement={() => {}} deleteElement={() => {}}/>
+                break;
+        }        
+        
+        setElements([...elements]);
     }
 
-    const onCloseAddBlock = (id:string) =>
+    const deleteElement = (id:any) =>
     {
-        let filtered = addBlock.filter(x => x !== id)
-        let filteredContent = content.filter((x:any) => x.id !== id)
-        setContent([...filteredContent])
-        setAddBlock(filtered);
+        const newElements = elements.filter(x => x.props.config.id !== id)
+        setElements([...newElements]);
     }
+    
 
     return (
-        <div className={styles.main}>
-            <div className={styles.block}>
+        <div className={styles.main} key={'CREATE'}>
+            <FloatMenu createElement={createElement} preview={preview} setPreview={setPreview}/>
+            <div className={styles.block} id='create'>
                 <h1 className={styles.header}>Create</h1>
-                <div className={styles.information}>
-                    <input className={styles.input} onChange={titleHandler} placeholder='Title' type="text" name="" id="" />
-                    <input className={styles.input} onChange={headerImageHandler} type="file" accept="image/png, image/jpeg"/>
-                    <textarea className={styles.description} onChange={descriptionHandler} name="" id="" placeholder='Enter a short Desciption...'></textarea>
-                    <div className={styles.tag_select}>
-                        <div className={styles.tag} onClick={tagHandler} key='Research'>
-                            <input id='abc' type="checkbox" hidden /> Research
-                        </div>
-                        <div className={styles.tag} onClick={tagHandler} key='Presentation'>
-                            <input id='abc' type="checkbox" hidden /> Presentation
-                        </div>
-                        <div className={styles.tag} onClick={tagHandler} key='Design'>
-                            <input id='abc' type="checkbox" hidden /> Design
-                        </div>
-                    </div>
-                </div>
-                <div id='editor-block'>
-                    {addBlock.map((x:any) => {return <AddContent key={x} id={x} onCloseAddBlock={onCloseAddBlock} content={content} setContent={setContent}/>})}
-                </div>
-                <div className={styles.add_block} onClick={onClickAddBlock}>
-                    <h1>+</h1>
+                <label>
+                    Preview: <input type="checkbox" id="preview" />
+                </label>
+                <Header setHeader={setHeader} header={header}/>
+                <div id='add_elements'>
+                    {elements.map((e) => 
+                    { 
+                        switch(e.props.config.type)
+                        {
+                            case 'image':
+                                return <div><CreateImage config={e.props.config} updateElement={updateElement} deleteElement={deleteElement}/></div>
+                            case 'header':
+                                return <div><CreateHeader config={e.props.config} updateElement={updateElement} deleteElement={deleteElement}/></div>
+                            case 'content':
+                                return <div><CreateContent config={e.props.config} updateElement={updateElement} deleteElement={deleteElement}/></div>
+                        }
+                    })}
                 </div>
             </div>
-            <PreviewArticle headerImage={headerImage} title={title} description={description} tags={tags} content={content}/>
+            {preview && (
+                <PreviewBlogEntry header={header}>
+                    {elements.map((e) =>  
+                    {   
+                        switch(e.props.config.type)
+                        {
+                            case 'image':
+                                return <div ><PreviewImage config={e.props.config}/></div>
+                            case 'header':
+                                return <div><PreviewHeader config={e.props.config}/></div>
+                            case 'content':
+                                return <div><PreviewContent config={e.props.config}/></div>
+                        }
+                    })}
+                </PreviewBlogEntry>
+            )}
         </div>
     )
 }
