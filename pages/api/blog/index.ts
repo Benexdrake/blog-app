399ea9e680332ec75e109d2f2b2fd51bc38fa34a
BeneX Drake <benex@hotmail.de>
2025-01-25 20:32:01 +0100

    update

---
+++ pages/api/blog/index.ts
@@ -0,0 +1,171 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler( req: NextApiRequest, res: NextApiResponse ) 
+{
+    res.status(200).json(articles)
+}
+
+const articles = [
+    {
+      id: 1,
+      title: "I ♥ Javascript",
+      content: "This is the content of article 1",
+      image: {
+        src: "JS.jpg",
+        alt: "I love Javascript",
+      },
+      link: "#i-love-javascript",
+      teaser: "This is the reason why I love Javascript!",
+      date: "2021-01-01",
+      author: "John Doe",
+      tags: [
+        {
+          id: 1,
+          name: "Design",
+          bgColor: "#F9F5FF",
+          textColor: "#6941C6",
+        },
+        {
+          id: 2,
+          name: "Research",
+          bgColor: "#EEF4FF",
+          textColor: "#3538CD",
+        },
+        {
+          id: 3,
+          name: "Presentation",
+          bgColor: "#FDF2FA",
+          textColor: "#C11574",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "CSS is Awesome",
+      content: "This is the content of article 2",
+      image: {
+        src: "TS.jpg",
+        alt: "Picture with Love",
+      },
+      teaser: "This is the reason why I love CSS!",
+      date: "2021-01-02",
+      author: "Jane Doe",
+      tags: [
+        {
+          id: 1,
+          name: "Design",
+          bgColor: "#F9F5FF",
+          textColor: "#6941C6",
+        },
+        {
+          id: 2,
+          name: "Research",
+          bgColor: "#EEF4FF",
+          textColor: "#3538CD",
+        },
+        {
+          id: 3,
+          name: "Presentation",
+          bgColor: "#FDF2FA",
+          textColor: "#C11574",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "HTML is Cool",
+      content: "This is the content of article 3",
+      image: {
+        src: "HTML.jpg",
+        alt: "Picture with code",
+      },
+      teaser: "This is the reason why I love HTML!",
+      date: "2021-01-03",
+      author: "John Doe",
+      tags: [
+        {
+          id: 1,
+          name: "Design",
+          bgColor: "#F9F5FF",
+          textColor: "#6941C6",
+        },
+        {
+          id: 2,
+          name: "Research",
+          bgColor: "#EEF4FF",
+          textColor: "#3538CD",
+        },
+        {
+          id: 3,
+          name: "Presentation",
+          bgColor: "#FDF2FA",
+          textColor: "#C11574",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "React is the Best",
+      content: "This is the content of article 4",
+      image: {
+        src: "CSharp.jpg",
+        alt: "View from window to a river",
+      },
+      teaser: "This is the reason why I love React!",
+      date: "2021-01-04",
+      author: "Jane Doe",
+      tags: [
+        {
+          id: 1,
+          name: "Design",
+          bgColor: "#F9F5FF",
+          textColor: "#6941C6",
+        },
+        {
+          id: 2,
+          name: "Research",
+          bgColor: "#EEF4FF",
+          textColor: "#3538CD",
+        },
+        {
+          id: 3,
+          name: "Presentation",
+          bgColor: "#FDF2FA",
+          textColor: "#C11574",
+        },
+      ],
+    },
+    {
+        id: 5,
+        title: "React is the Best",
+        content: "This is the content of article 4",
+        image: {
+          src: "Python.png",
+          alt: "View from window to a river",
+        },
+        teaser: "This is the reason why I love React!",
+        date: "2021-01-04",
+        author: "Jane Doe",
+        tags: [
+          {
+            id: 1,
+            name: "Design",
+            bgColor: "#F9F5FF",
+            textColor: "#6941C6",
+          },
+          {
+            id: 2,
+            name: "Research",
+            bgColor: "#EEF4FF",
+            textColor: "#3538CD",
+          },
+          {
+            id: 3,
+            name: "Presentation",
+            bgColor: "#FDF2FA",
+            textColor: "#C11574",
+          },
+        ],
+      },
+  ];
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,50 @@
+import styles from '@/styles/modules/header.module.css';
+
 import { Header } from "@/components/header/header";
 import { AllPosts } from "@/layouts/blog/all_posts";
 import { RecentPosts } from "@/layouts/blog/recent_posts";
 
-import styles from '@/styles/modules/header.module.css'
-import { Project } from "@/types/project";
-import axios from "axios";
 import { GetServerSidePropsContext } from "next";
+import { getAllArticles } from "@/services/article_service";
+import { Article } from "@/types/article";
 
-interface ProjectsProps
+interface BlogProps
 {
-  projects:Project[];
+  articles:Article[];
   page:number;
-  max_repos:number;
 }
 
-export default function Home(props:ProjectsProps)
-{
-  const projects = props.projects as Project[];
-  const page = props.page as number;
-  const max_repos = props.max_repos;
+export default function Home(props: BlogProps) {
+  const articles = props.articles;
+  let page = props.page | 1;
 
   return (
-   <div className="main">
-     <Header styles={styles} header='projects'/>
-    {
-      page == 1 ?
-      <>
-      <br />
-      <RecentPosts projects={projects.slice(0,4)}/>
-      <br />
-      <AllPosts projects={projects.slice(3, projects.length)}/>
-      </>
-      :
-      <>
-     <br />
-     <AllPosts projects={projects}/> 
-      </>
-    }
-   </div>
+    <div className="main">
+      <Header styles={styles} header='the blog' />
+      {
+        page == 1 ?
+          <>
+            <br />
+            <RecentPosts articles={articles.slice(0, 4)} />
+            <br />
+            <AllPosts articles={articles.slice(3, articles.length)} />
+          </>
+          :
+          <>
+            <AllPosts articles={articles} />
+          </>
+      }
+    </div>
   );
 }
 
-export async function getServerSideProps(context:GetServerSidePropsContext)
+export async function getServerSideProps(context: GetServerSidePropsContext)
 {
-  let page = context.query.page || 1
-  const response = await axios.get(`http://${context.req.headers.host}/api/github?page=${page}`).then((x:any) => {return x.data})
+  const articles = await getAllArticles(context.req.headers.host || '');
   
   return {
     props: {
-      max_repos: response.max_repos,
-      projects:response.repos,
-      page
+      articles,
     }
   }
 };
