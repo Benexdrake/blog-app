cb534bcf3cc2982a234423800aaece05eb6f841b
BeneX Drake <benex@hotmail.de>
2025-01-26 14:51:24 +0100

    removing mongoose

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,7 +1,7 @@
 import styles from '@/styles/modules/blog/create/create_blog.module.css'
 import { JSX, useState} from 'react'
 
-import { createArticle } from '@/data/db_context';
+// import { createArticle } from '@/data/db_context';
 
 import { BlogElementConfig } from '@/types/blog_element_config';
 import {CreateArticle} from '@/types/create_article';
@@ -30,7 +30,7 @@ export default function _()
         const e = elements.map(x => {return x.props.config})
         const new_article:CreateArticle = {id, headerImage:header.image, title: header.title, description:header.description, tags:header.tags, elements:e}
 
-        await createArticle(new_article);
+        // await createArticle(new_article);
     }
     
     const createElement = (type:string) =>
