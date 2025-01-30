a6a3e338ed0e120aaacdbf5a187eac51897346a8
BeneX Drake <benex@hotmail.de>
2025-01-30 16:59:29 +0100

    mongoosh test works, next step CRUD

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -1,14 +1,39 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+//DB
+const dbUrl = process.env.DB
+mongoose.connect(dbUrl).then(() => {console.log('HI from DB')}) 
+
+const userSchema = new mongoose.Schema(
+{
+    name:String,
+    age:Number,
+});
+
+const UserModel = mongoose.model("users", userSchema)
+
 
 const app = express();
-
 app.use(express.json())
 app.use(cors())
 
-app.listen(3000, () => console.log('Server is running...'));
+const PORT = process.env.PORT || 8000
 
-app.get('/', (req, res) => 
+app.listen(PORT, () => console.log('Server is running...'));
+
+app.get('/', async (req, res) => 
 {
-    return res.status(200).send('HALLO')
+    const user = await UserModel.create({
+        name:'HALLO',
+        age:1111
+    })
+
+    const userData = await UserModel.find();
+    return res.status(200).send(userData)
 })
